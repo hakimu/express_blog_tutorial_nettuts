603ce11ebef116eeebb90683d271673d448f8a46
Hakimu Jackson <hakimu@newrelic.com>
2014-09-13 08:46:25 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('newrelic');
+var express = require('express');
+var app = express();
+
+var hbs = require('hbs');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+app.get('/', function(req, res){
+	res.render('index')
+});
+
+app.get('/about', function(req, res){
+	res.render('about')
+});
+
+app.get('/article', function(req, res){
+	res.render('article')
+});
+
+app.listen(3000);
+console.log('Listening on port...3000')
